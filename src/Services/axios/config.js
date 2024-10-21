61f356485befd6fea14ccd4fab3f47f7d94f3120
Nguyen Xuan Duc <nguyenxuanduc280703@gmail.com>
2024-10-21 15:05:48 +0700

    [fix] deploy

--- src/Services/axios/config.js
+++ src/Services/axios/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'https://localhost:7203',
+  baseURL: 'https://opal.io.vn/',
   headers: {
     'Content-type': 'application/json',
   },
